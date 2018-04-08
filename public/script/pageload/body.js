782c87103e89d8f38dbb62be20e55e4e3139364a
gasolin <gasolin@gmail.com>
2018-04-08 16:16:37 +0800

    simplify body check flow
    pick from 57767a7

--- public/script/pageload/body.js
+++ public/script/pageload/body.js
@@ -1,44 +1,47 @@
 
 
 //injects various information on the page
-var body = function(){
-    var url = document.location.href;
-    var isOnGitHubcom = url.indexOf('://github.com') != -1 && url.indexOf('://github.com') < 15;
+var body = function(url) {
+    var isOnGitHubcom = url.indexOf('://github.com') !== -1 && url.indexOf('://github.com') < 15;
     var isOnGitHubEnterprise = document.getElementsByClassName('enterprise').length >= 1 && document.getElementsByClassName('header-logo-invertocat').length >= 1;
     var isOnGitHub = isOnGitHubEnterprise || isOnGitHubcom;
 
-    if(!isOnGitHub){
+    if(!isOnGitHub) {
         setThumbnail('');
     }
 
-    var isOnUserProfile = isOnGitHub && url.match(/.+\/.+\/?/gi) != null && document.getElementById("report-block-modal");
-    var isOnProfile = isOnGitHub && url.match(/.+\/.+\/.+\/?/gi) != null;
-    var isOnRepo = isOnGitHub && url.match(/.+\/.+\/.+\/.+\/?/gi) != null;
-    var isOnIssuePage = isOnGitHub && ( url.indexOf('/pull/') != -1 || url.indexOf('/issue/') != -1 || url.indexOf('/issues/') != -1 );
-    var isOnIssuesPage = isOnGitHub && url.indexOf('/issues') != -1;
+    var isOnUserProfile = url.match(/.+\/.+\/?/gi) !== null && document.getElementById("report-block-modal");
+    var isOnProfile = url.match(/.+\/.+\/.+\/?/gi) !== null;
+    var isOnRepo = url.match(/.+\/.+\/.+\/.+\/?/gi) !== null;
+    var isOnIssuePage = url.indexOf('/pull/') !== -1 || url.indexOf('/issue/') !== -1 || url.indexOf('/issues/') !== -1;
+    var isOnIssuesPage = url.indexOf('/issues') !== -1;
     var isAlreadyGitcoinBounty = document.getElementsByClassName('gitcoin_bounty').length >= 1;
-    var isOnIssueBoard = isOnGitHub && url.indexOf('boards') != -1;
+    var isOnIssueBoard = isOnGitHub && url.indexOf('boards') !== -1;
 
     // Location | User Profile
-    if(isOnUserProfile && !isAlreadyGitcoinBounty ){
-        addButtonToUserPage();
+    if(isOnUserProfile && !isAlreadyGitcoinBounty ) {
+      // console.log(isAlreadyGitcoinBounty);
+      addButtonToUserPage();
     }
+
     // Location | Issue Page
-    if (isOnIssuePage && !isAlreadyGitcoinBounty){
-            addButtonToIssuePage();
-            addBountyInfoToIssuePage(url);
-            injectGetBountyAmount();
-    } else if (isOnRepo){
-        var repoUrl = document.location.href.split('issues')[0].split('pulls')[0];
-        injectGetNumberBounties(repoUrl);
-    } else if (isOnGitHub){
-        injectGetTotalBounties();
-    } 
+    if (isOnIssuePage && !isAlreadyGitcoinBounty) {
+      // console.log(isAlreadyGitcoinBounty);
+      addButtonToIssuePage();
+      addBountyInfoToIssuePage(url);
+      injectGetBountyAmount();
+    } else if (isOnRepo) {
+      var repoUrl = document.location.href.split('issues')[0].split('pulls')[0];
+      injectGetNumberBounties(repoUrl);
+    } else {
+      injectGetTotalBounties();
+    }
+
     if (isOnIssuesPage) {
-        injectGetAllBountiesOnIssuesPage();
+      injectGetAllBountiesOnIssuesPage();
     }
     if (isOnIssueBoard) {
-        injectGetAllBountiesOnIssueBoard(); 
+      injectGetAllBountiesOnIssueBoard();
     }
 }
 
@@ -46,11 +49,9 @@ var url = window.location.href;
 var hasRun = false;
 
 setInterval(function() {
-    if (url !== window.location.href || !hasRun) {
-        url = window.location.href;
-        body();
-    }
-    hasRun = true;
+  if (url !== window.location.href || !hasRun) {
+      url = window.location.href;
+      body(url);
+  }
+  hasRun = true;
 }, 2000);
-
-
